28d71d80c68511ce44cdd86115ffca58f6930671
Ruslan Khaertdinov <ruslan.khaertdinov@flatstack.com>
2016-07-19 23:03:48 +0300

    add basic view

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,7 +1,22 @@
 class FeedbacksController < ApplicationController
   def new
+    @feedback = Feedback.new
   end
 
   def create
+    byebug
+    @feedback = Feedback.new(feedback_params)
+
+    if @feedback.valid?
+      puts "Valid!!!!!!!"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(*Feedback::ATTRIBUTES)
   end
 end
